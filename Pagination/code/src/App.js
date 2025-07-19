5d3764b41f0ce609bef51b836df36c296c8eec99
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2025-07-20 02:15:40 +0530

    pagination with loading data functionality

--- Pagination/code/src/App.js
+++ Pagination/code/src/App.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Pagination from "./components/pagination/Pagination.jsx";
 
 const data = Array.from({ length: 100 }, (_, index) => index + 1);
 
 const App = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setData(Array.from({ length: 10 }, (_, index) => index + 1));
+  }, []);
+
+  const handlePageChange = (pageNumber) => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      setData(Array.from({ length: 10 }, (_, index) => index * pageNumber + 1));
+    }, 3000);
+  };
+
   return (
     <div>
       <Pagination
+        loading={loading}
         data={data}
+        totalNumberOfPages={100}
         renderRow={(rowData, rowIndex) => {
           return (
             <div>{`Hello from App  Data = ${rowData}, Index = ${rowIndex}.`}</div>
           );
         }}
+        onPageChange={handlePageChange}
       />
     </div>
   );
--- Pagination/code/src/components/pagination/Pagination.jsx
+++ Pagination/code/src/components/pagination/Pagination.jsx
@@ -2,33 +2,65 @@ import React, { useState } from "react";
 
 const PAGE_SIZE = 10;
 const DEFAULT_PAGE = 1;
+const MAX_BUTTONS_DISPLAY = 5;
 
 const Pagination = ({
+  loading,
   data,
+  totalNumberOfPages = null,
   renderRow,
   rowPerPage = PAGE_SIZE,
   className = "",
+  onPageChange = () => {},
 }) => {
-  const [currentPage, setCurrentPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(DEFAULT_PAGE);
   const [pageSize, setPageSize] = useState(rowPerPage);
 
-  const startIndex = (currentPage - 1) * pageSize;
+  const startIndex = totalNumberOfPages ? 0 : (currentPage - 1) * pageSize;
   const endIndex = startIndex + pageSize;
   const visibleData = data.slice(startIndex, endIndex);
-  const totalPages = Math.ceil(data.length / pageSize);
+  const totalPages = totalNumberOfPages ?? Math.ceil(data.length / pageSize);
+  const maxButtons = MAX_BUTTONS_DISPLAY;
+  let buttonStartIndex = currentPage - Math.floor(maxButtons / 2);
+  let buttonEndIndex = currentPage + Math.floor(maxButtons / 2);
 
   const pageNumberButtons = Array.from(
     { length: totalPages },
     (_, index) => index + 1
   );
 
+  if (buttonStartIndex < 1) {
+    buttonStartIndex = 1;
+    buttonEndIndex = Math.min(totalPages, maxButtons);
+  }
+
+  if (buttonEndIndex > totalPages) {
+    buttonEndIndex = totalPages;
+    buttonStartIndex = Math.max(1, totalPages - maxButtons + 1);
+  }
+
+  const buttonsToDisplay = pageNumberButtons.slice(
+    buttonStartIndex - 1,
+    buttonEndIndex
+  );
+
+  const handlePageChange = (pageNumber) => {
+    if (pageNumber < 1 || pageNumber > totalPages) return;
+    setCurrentPage(pageNumber);
+    onPageChange(pageNumber);
+  };
+
+  console.log(buttonStartIndex, buttonEndIndex, totalPages);
   return (
     <div className={`pagination ${className}`}>
-      <div className="pagination-content">
-        {visibleData.map((item, index) => (
-          <div key={index}>{renderRow(item, index)}</div>
-        ))}
-      </div>
+      {!loading && (
+        <div className="pagination-content">
+          {visibleData.map((item, index) => (
+            <div key={index}>{renderRow(item, index)}</div>
+          ))}
+        </div>
+      )}
+      {loading && <div className="loading">Loading...</div>}
       <div className="pagination-footer">
         <select onChange={(e) => setPageSize(Number(e.target.value))}>
           <option value={10}>10</option>
@@ -37,19 +69,22 @@ const Pagination = ({
           <option value={40}>40</option>
           <option value={50}>50</option>
         </select>
-        <button onClick={() => setCurrentPage(1)} disabled={currentPage === 1}>
+        <button
+          onClick={() => handlePageChange(1)}
+          disabled={currentPage === 1}
+        >
           First
         </button>
         <button
-          onClick={() => setCurrentPage(currentPage - 1)}
+          onClick={() => handlePageChange(currentPage - 1)}
           disabled={currentPage === 1}
         >
           Previous
         </button>
-        {pageNumberButtons.map((pageNumber) => (
+        {buttonsToDisplay.map((pageNumber) => (
           <button
             key={pageNumber}
-            onClick={() => setCurrentPage(pageNumber)}
+            onClick={() => handlePageChange(pageNumber)}
             style={{
               backgroundColor: currentPage === pageNumber ? "pink" : "yellow",
             }}
@@ -58,7 +93,7 @@ const Pagination = ({
           </button>
         ))}
         <button
-          onClick={() => setCurrentPage(currentPage + 1)}
+          onClick={() => handlePageChange(currentPage + 1)}
           disabled={currentPage === totalPages}
         >
           Next
@@ -66,7 +101,7 @@ const Pagination = ({
 
         <button
           disabled={currentPage === totalPages}
-          onClick={() => setCurrentPage(totalPages)}
+          onClick={() => handlePageChange(totalPages)}
         >
           Last
         </button>
