be4e6c77ebf5735c4622bcedcfec6f161fb34c63
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2025-07-23 03:25:46 +0530

    file explorer with folder and file adding functionality.

--- file-explorer/code/src/components/file-explorer/FileExplorer.jsx
+++ file-explorer/code/src/components/file-explorer/FileExplorer.jsx
@@ -1,30 +1,218 @@
-import React from "react";
+import React, { useState } from "react";
 
 const FileExplorer = ({ data }) => {
+  const [explorer, setExplorer] = useState(structuredClone(data));
+
+  function handleExpand(path) {
+    return () => {
+      const arrayIndexes = path
+        .split("/")
+        .filter((v) => !!v)
+        .map(Number);
+      console.log(path, arrayIndexes);
+      const newExplorer = structuredClone(explorer);
+      const secondLastElement = arrayIndexes
+        .slice(0, -1)
+        .reduce((acc, next) => {
+          return acc[next].children;
+        }, newExplorer);
+
+      const lastIndex = arrayIndexes.at(-1);
+      const lastElement = secondLastElement[lastIndex];
+
+      lastElement.isExpanded = !lastElement.expand;
+
+      setExplorer(newExplorer);
+    };
+  }
+
+  function handleAddFolder(path) {
+    return () => {
+      const arrayIndexes = path
+        .split("/")
+        .filter((v) => !!v)
+        .map(Number);
+      const newExplorer = structuredClone(explorer);
+      const secondLastElement = arrayIndexes
+        .slice(0, -1)
+        .reduce((acc, next) => {
+          return acc[next].children;
+        }, newExplorer);
+
+      const lastIndex = arrayIndexes.at(-1);
+      const lastElement = secondLastElement[lastIndex];
+
+      lastElement.isExpanded = true;
+
+      lastElement.children = [
+        {
+          id: new Date().getTime(),
+          type: "add-folder",
+        },
+        ...lastElement.children,
+      ];
+
+      setExplorer(newExplorer);
+    };
+  }
+
+  function handleAddFile(path) {
+    return () => {
+      const arrayIndexes = path
+        .split("/")
+        .filter((v) => !!v)
+        .map(Number);
+      const newExplorer = structuredClone(explorer);
+      const secondLastElement = arrayIndexes
+        .slice(0, -1)
+        .reduce((acc, next) => {
+          return acc[next].children;
+        }, newExplorer);
+
+      const lastIndex = arrayIndexes.at(-1);
+      const lastElement = secondLastElement[lastIndex];
+
+      lastElement.isExpanded = true;
+
+      lastElement.children = [
+        {
+          id: new Date().getTime(),
+          type: "add-file",
+        },
+        ...lastElement.children,
+      ];
+
+      setExplorer(newExplorer);
+    };
+  }
+
+  function handleKeyDown(path) {
+    return (e) => {
+      if (e.key === "Enter") {
+        const folderName = e.target.value;
+        const arrayIndexes = path
+          .split("/")
+          .filter((v) => !!v)
+          .map(Number);
+        const newExplorer = structuredClone(explorer);
+        const secondLastElement = arrayIndexes
+          .slice(0, -1)
+          .reduce((acc, next) => {
+            return acc[next].children;
+          }, newExplorer);
+
+        const newFolder = {
+          id: new Date().getTime(),
+          name: folderName,
+          type: "folder",
+          children: [],
+        };
+
+        secondLastElement[0] = newFolder;
+        setExplorer(newExplorer);
+      }
+    };
+  }
+
+  function handleKeyDownFile(path) {
+    return (e) => {
+      if (e.key === "Enter") {
+        const fileName = e.target.value;
+        const arrayIndexes = path
+          .split("/")
+          .filter((v) => !!v)
+          .map(Number);
+        const newExplorer = structuredClone(explorer);
+        const secondLastElement = arrayIndexes
+          .slice(0, -1)
+          .reduce((acc, next) => {
+            return acc[next].children;
+          }, newExplorer);
+
+        const newFile = {
+          id: new Date().getTime(),
+          name: fileName,
+          type: "file",
+        };
+
+        secondLastElement[0] = newFile;
+        setExplorer(newExplorer);
+      }
+    };
+  }
+
   return (
     <div>
-      <Tree data={data} />
+      <Tree
+        data={explorer}
+        handleExpand={handleExpand}
+        handleAddFolder={handleAddFolder}
+        handleAddFile={handleAddFile}
+        handleKeyDown={handleKeyDown}
+        handleKeyDownFile={handleKeyDownFile}
+      />
     </div>
   );
 };
 
 export default FileExplorer;
 
-const Tree = ({ data }) => {
+const Tree = ({
+  data,
+  path = "",
+  handleExpand,
+  handleAddFolder,
+  handleAddFile,
+  handleKeyDown,
+  handleKeyDownFile,
+}) => {
   return (
     <div className="tree">
       {data.map((folderData, index) => {
+        const localPath = `${path}/${index}`;
         const icon = folderData.type === "folder" ? "📁" : "📄";
         const heighlightText = folderData.type === "folder" ? "text-bold" : "";
         return (
           <div key={index}>
-            <div>
-              {icon}
-              <span className={`${heighlightText}`}>{folderData.name}</span>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <div onClick={handleExpand(localPath)}>
+                {!!folderData.name && <span>{icon}</span>}
+                <span className={`${heighlightText}`}>{folderData.name}</span>
+              </div>
+              {folderData.type === "folder" && (
+                <>
+                  <button onClick={handleAddFolder(localPath)}>+📂</button>
+                  <button onClick={handleAddFile(localPath)}>+📝</button>
+                </>
+              )}
             </div>
-            {folderData.type === "folder" && folderData.children && (
-              <Tree data={folderData.children} />
+            {folderData.type === "add-folder" && (
+              <input
+                autoFocus
+                onKeyDown={handleKeyDown(localPath)}
+                type="text"
+              />
             )}
+            {folderData.type === "add-file" && (
+              <input
+                autoFocus
+                onKeyDown={handleKeyDownFile(localPath)}
+                type="text"
+              />
+            )}
+            {folderData.isExpanded &&
+              folderData.type === "folder" &&
+              folderData.children && (
+                <Tree
+                  data={folderData.children}
+                  path={localPath}
+                  handleExpand={handleExpand}
+                  handleAddFolder={handleAddFolder}
+                  handleAddFile={handleAddFile}
+                  handleKeyDown={handleKeyDown}
+                  handleKeyDownFile={handleKeyDownFile}
+                />
+              )}
           </div>
         );
       })}
